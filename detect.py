281d8d678071e72421ae57fc158ba2ff297544f2
wangzhesun <wangzhe.sun@outlook.com>
2022-05-31 00:28:59 -0500

    update structures

--- detect.py
+++ detect.py
@@ -4,9 +4,7 @@ import time
 import torch
 from torch.autograd import Variable
 from util import util
-import argparse
 import os
-import os.path as osp
 from network import Darknet
 import pickle as pkl
 import pandas as pd
@@ -14,155 +12,14 @@ import cv2 as cv
 import random
 
 
-def arg_parse():
+def write_img(x, results):
     """
-    Parse arguments to the detect module
+    draw bounding boxes and prediction class on images
 
+    :param x: prediction output
+    :param results: original images
+    :return: images with prediction boxes
     """
-
-    parser = argparse.ArgumentParser(description='YOLO v3 Detection Module')
-
-    parser.add_argument("--images", dest='images', help="Image / imgs",
-                        default="imgs", type=str)
-    parser.add_argument("--det", dest='det', help="Image / det",
-                        default="det", type=str)
-    parser.add_argument("--bs", dest="bs", help="Batch size", default=1)
-    parser.add_argument("--confidence", dest="confidence",
-                        help="Object Confidence to filter predictions", default=0.5)
-    parser.add_argument("--nms_thresh", dest="nms_thresh", help="NMS Threshhold", default=0.4)
-    parser.add_argument("--cfg", dest='cfgfile', help="Config file", default="cfg/yolov3.cfg",
-                        type=str)
-    parser.add_argument("--weights", dest='weightsfile', help="weightsfile",
-                        default="weights/yolov3.weights", type=str)
-    parser.add_argument("--reso", dest='reso', help=
-    "Input resolution of the network. Increase to increase accuracy. Decrease to increase speed",
-                        default="416", type=str)
-
-    return parser.parse_args()
-
-
-args = arg_parse()
-images = args.images
-batch_size = int(args.bs)
-confidence = float(args.confidence)
-nms_thresh = float(args.nms_thresh)
-start = 0
-
-num_classes = 80
-classes = util.load_classes("data/coco.names.txt")
-
-# Set up the neural network
-print("Loading network.....")
-model = Darknet(args.cfgfile)
-model.load_weights(args.weightsfile)
-print("Network successfully loaded")
-
-model.net_info["height"] = args.reso
-inp_dim = int(model.net_info["height"])
-assert inp_dim % 32 == 0
-assert inp_dim > 32
-
-# Set the model in evaluation mode
-model.eval()
-
-read_dir = time.time()
-# Detection phase
-try:
-    imlist = [osp.join(osp.realpath('.'), images, img) for img in os.listdir(images)]
-except NotADirectoryError:
-    imlist = [osp.join(osp.realpath('.'), images)]
-except FileNotFoundError:
-    print("No file or directory with the name {}".format(images))
-    exit()
-
-if not os.path.exists(args.det):
-    os.makedirs(args.det)
-
-load_batch = time.time()
-loaded_ims = [cv.imread('.' + x[69:]) for x in imlist]
-
-im_batches = list(map(util.prep_image, loaded_ims, [inp_dim for x in range(len(imlist))]))
-im_dim_list = [(x.shape[1], x.shape[0]) for x in loaded_ims]
-im_dim_list = torch.FloatTensor(im_dim_list).repeat(1, 2)
-
-leftover = 0
-if len(im_dim_list) % batch_size:
-    leftover = 1
-
-if batch_size != 1:
-    num_batches = len(imlist) // batch_size + leftover
-    im_batches = [torch.cat((im_batches[i * batch_size: min((i + 1) * batch_size,
-                                                            len(im_batches))])) for i in
-                  range(num_batches)]
-
-write = 0
-
-start_det_loop = time.time()
-for i, batch in enumerate(im_batches):
-    # load the image
-    start = time.time()
-    with torch.no_grad():
-        prediction = model(Variable(batch))
-
-    prediction = util.write_results(prediction, confidence, nms_thresh, num_classes)
-
-    end = time.time()
-
-    if type(prediction) == int:
-
-        for im_num, image in enumerate(
-                imlist[i * batch_size: min((i + 1) * batch_size, len(imlist))]):
-            im_id = i * batch_size + im_num
-            print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1],
-                                                                 (end - start) / batch_size))
-            print("{0:20s} {1:s}".format("Objects Detected:", ""))
-            print("----------------------------------------------------------")
-        continue
-
-    prediction[:,
-    0] += i * batch_size  # transform the atribute from index in batch to index in imlist
-
-    if not write:  # If we have't initialised output
-        output = prediction
-        write = 1
-    else:
-        output = torch.cat((output, prediction))
-
-    for im_num, image in enumerate(imlist[i * batch_size: min((i + 1) * batch_size, len(imlist))]):
-        im_id = i * batch_size + im_num
-        objs = [classes[int(x[-1])] for x in output if int(x[0]) == im_id]
-        print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1],
-                                                             (end - start) / batch_size))
-        print("{0:20s} {1:s}".format("Objects Detected:", " ".join(objs)))
-        print("----------------------------------------------------------")
-
-try:
-    output
-except NameError:
-    print("No detections were made")
-    exit()
-
-im_dim_list = torch.index_select(im_dim_list, 0, output[:, 0].long())
-
-scaling_factor = torch.min(416 / im_dim_list, 1)[0].view(-1, 1)
-
-output[:, [1, 3]] -= (inp_dim - scaling_factor * im_dim_list[:, 0].view(-1, 1)) / 2
-output[:, [2, 4]] -= (inp_dim - scaling_factor * im_dim_list[:, 1].view(-1, 1)) / 2
-
-output[:, 1:5] /= scaling_factor
-
-for i in range(output.shape[0]):
-    output[i, [1, 3]] = torch.clamp(output[i, [1, 3]], 0.0, im_dim_list[i, 0])
-    output[i, [2, 4]] = torch.clamp(output[i, [2, 4]], 0.0, im_dim_list[i, 1])
-
-output_recast = time.time()
-class_load = time.time()
-colors = pkl.load(open("pallete", "rb"))
-
-draw = time.time()
-
-
-def write(x, results):
     c1 = tuple(x[1:3].int())
     c2 = tuple(x[3:5].int())
     img = results[int(x[0])]
@@ -182,23 +39,155 @@ def write(x, results):
     return img
 
 
-list(map(lambda x: write(x, loaded_ims), output))
+def detect(src, det, batch_size=1, confidence=0.5, nms_thresh=0.4):
+    """
+    run yolo v3 on the input images provided by src and output results to det
 
-det_names = pd.Series(imlist).apply(lambda x: "{}/det_{}".format(args.det, x.split("\\")[-1]))
+    :param src: path to the source images: will deal with all images if the path is a directory
+    :param det: destination path for output images
+    :param batch_size: batch size, default = 1
+    :param confidence: confidence threshold, default = 0.5
+    :param nms_thresh: non-maximum suppression threshold, default = 0.4
+    """
+    read_dir = time.time()
+    try: # try if the src is a directory path
+        imlist = [src+'/'+img for img in os.listdir(src)]
+    except NotADirectoryError: # try file path if it is not a directory
+        imlist = [src]
+    except FileNotFoundError: # invalid path if it is neither directory nor file
+        print("No file or directory with the name {}".format(src))
+        exit()
 
-list(map(cv.imwrite, det_names, loaded_ims))
+    # make the destination directory if not exist already
+    if not os.path.exists(det):
+        os.makedirs(det)
 
-end = time.time()
+    load_batch = time.time()
+    loaded_ims = [cv.imread(x) for x in imlist]
 
-print("SUMMARY")
-print("----------------------------------------------------------")
-print("{:25s}: {}".format("Task", "Time Taken (in seconds)"))
-print()
-print("{:25s}: {:2.3f}".format("Reading addresses", load_batch - read_dir))
-print("{:25s}: {:2.3f}".format("Loading batch", start_det_loop - load_batch))
-print("{:25s}: {:2.3f}".format("Detection (" + str(len(imlist)) + " images)",
-                               output_recast - start_det_loop))
-print("{:25s}: {:2.3f}".format("Output Processing", class_load - output_recast))
-print("{:25s}: {:2.3f}".format("Drawing Boxes", end - draw))
-print("{:25s}: {:2.3f}".format("Average time_per_img", (end - load_batch) / len(imlist)))
-print("----------------------------------------------------------")
+    # preprocessing input images
+    im_batches = list(map(util.prep_image, loaded_ims, [inp_dim for x in range(len(imlist))]))
+    im_dim_list = [(x.shape[1], x.shape[0]) for x in loaded_ims]
+    im_dim_list = torch.FloatTensor(im_dim_list).repeat(1, 2)
+
+    leftover = 0
+    if len(im_dim_list) % batch_size:
+        leftover = 1
+
+    if batch_size != 1:
+        num_batches = len(imlist) // batch_size + leftover
+        im_batches = [torch.cat((im_batches[i * batch_size: min((i + 1) * batch_size,
+                                                                len(im_batches))])) for i in
+                      range(num_batches)]
+
+    write = 0
+
+    start_det_loop = time.time()
+    for i, batch in enumerate(im_batches):
+        # load the image
+        start = time.time()
+        with torch.no_grad():
+            prediction = model(Variable(batch))
+
+        prediction = util.write_results(prediction, confidence, nms_thresh, num_classes)
+
+        end = time.time()
+
+        if type(prediction) == int:
+
+            for im_num, image in enumerate(
+                    imlist[i * batch_size: min((i + 1) * batch_size, len(imlist))]):
+                im_id = i * batch_size + im_num
+                print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1],
+                                                                     (end - start) / batch_size))
+                print("{0:20s} {1:s}".format("Objects Detected:", ""))
+                print("----------------------------------------------------------")
+            continue
+
+        # transform the attribute from index in batch to index in imlist
+        prediction[:, 0] += i * batch_size
+
+        if not write:  # If we haven't initialised output
+            output = prediction
+            write = 1
+        else:
+            output = torch.cat((output, prediction))
+
+        for im_num, image in enumerate(
+                imlist[i * batch_size: min((i + 1) * batch_size, len(imlist))]):
+            im_id = i * batch_size + im_num
+            objs = [classes[int(x[-1])] for x in output if int(x[0]) == im_id]
+            print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1],
+                                                                 (end - start) / batch_size))
+            print("{0:20s} {1:s}".format("Objects Detected:", " ".join(objs)))
+            print("----------------------------------------------------------")
+
+    try:
+        output
+    except NameError:
+        print("No detections were made")
+        exit()
+
+    im_dim_list = torch.index_select(im_dim_list, 0, output[:, 0].long())
+
+    scaling_factor = torch.min(416 / im_dim_list, 1)[0].view(-1, 1)
+
+    output[:, [1, 3]] -= (inp_dim - scaling_factor * im_dim_list[:, 0].view(-1, 1)) / 2
+    output[:, [2, 4]] -= (inp_dim - scaling_factor * im_dim_list[:, 1].view(-1, 1)) / 2
+
+    output[:, 1:5] /= scaling_factor
+
+    for i in range(output.shape[0]):
+        output[i, [1, 3]] = torch.clamp(output[i, [1, 3]], 0.0, im_dim_list[i, 0])
+        output[i, [2, 4]] = torch.clamp(output[i, [2, 4]], 0.0, im_dim_list[i, 1])
+
+    output_recast = time.time()
+    class_load = time.time()
+    colors = pkl.load(open("pallete", "rb"))
+
+    draw = time.time()
+
+    # draw bounding boxes
+    list(map(lambda x: write_img(x, loaded_ims), output))
+
+    imlist = [img.split('/')[-1] for img in imlist]
+
+    det_names = pd.Series(imlist).apply(lambda x: "{}/det_{}".format(det, x.split("\\")[-1]))
+
+    list(map(cv.imwrite, det_names, loaded_ims))
+
+    end = time.time()
+
+    print("SUMMARY")
+    print("----------------------------------------------------------")
+    print("{:25s}: {}".format("Task", "Time Taken (in seconds)"))
+    print()
+    print("{:25s}: {:2.3f}".format("Reading addresses", load_batch - read_dir))
+    print("{:25s}: {:2.3f}".format("Loading batch", start_det_loop - load_batch))
+    print("{:25s}: {:2.3f}".format("Detection (" + str(len(imlist)) + " images)",
+                                   output_recast - start_det_loop))
+    print("{:25s}: {:2.3f}".format("Output Processing", class_load - output_recast))
+    print("{:25s}: {:2.3f}".format("Drawing Boxes", end - draw))
+    print("{:25s}: {:2.3f}".format("Average time_per_img", (end - load_batch) / len(imlist)))
+    print("----------------------------------------------------------")
+
+
+if __name__ == '__main__':
+    num_classes = 80
+    classes = util.load_classes("data/coco.names.txt")
+
+    # Set up the neural network
+    print("Loading network.....")
+    model = Darknet("cfg/yolov3.cfg")
+    model.load_weights("weights/yolov3.weights")
+    print("Network successfully loaded")
+
+    model.net_info["height"] = 416
+    inp_dim = int(model.net_info["height"])
+    assert inp_dim % 32 == 0
+    assert inp_dim > 32
+
+    colors = pkl.load(open("pallete", "rb"))
+
+    # run yolo v3 detection
+    detect('./imgs', './det')
